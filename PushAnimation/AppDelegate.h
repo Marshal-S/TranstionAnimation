bdf7dab5467b1db9ab343dcc080ce5a4d48e52b5
Marshal-S <921871381@qq.com>
2018-02-03 15:40:30 +0800

    Initial Commit

---
+++ PushAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushAnimation
+//
+//  Created by Marshal on 2018/2/3.
+//  Copyright © 2018年 Marshal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PushAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PushAnimation
+//
+//  Created by Marshal on 2018/2/3.
+//  Copyright © 2018年 Marshal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
